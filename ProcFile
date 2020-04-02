43bcadf4a4df16b035613a1d0d23812a29096147
Siddharth Bhatore <sbhatore95@gmail.com>
2020-04-02 19:29:10 +0530

    Procfile

---
+++ ProcFile
@@ -0,0 +1,1 @@
+web: gunicorn credit_risk.wsgi
--- credit_risk/settings.py
+++ credit_risk/settings.py
@@ -127,5 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Configure Django App for Heroku.
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
