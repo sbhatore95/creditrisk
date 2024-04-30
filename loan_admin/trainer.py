9330d2986eaf5781c2b099b76cfdfb2f8934435a
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-30 20:50:21 +0530

    Update trainer.py

--- loan_admin/trainer.py
+++ loan_admin/trainer.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
