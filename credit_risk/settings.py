996a8ade0c163c54847d5af37f2b663e8635a0c1
= <=>
2024-05-03 18:08:38 +0530

    Update settings.py

--- credit_risk/settings.py
+++ credit_risk/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
