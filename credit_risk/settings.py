f8f966a24607d4a59bc8ceee5335142900b2e2b1
= <=>
2024-05-03 16:58:10 +0530

    Update settings.py

--- credit_risk/settings.py
+++ credit_risk/settings.py
@@ -129,5 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
