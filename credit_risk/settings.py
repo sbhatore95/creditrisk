b86a4ce4c9c59fef3bdc5f9660c2eb077397de71
= <=>
2024-05-04 11:26:32 +0530

    Update settings.py

--- credit_risk/settings.py
+++ credit_risk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='bsnl1234'
+SECRET_KEY ='b36ae77f582b8ce32854eb208c2ffd2d677071a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
